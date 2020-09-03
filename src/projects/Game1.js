26d65c7d1df80ba9702cbcf2670f59313e9b6253
Revin Tanri <tanrirevin@gmail.com>
2020-09-03 23:15:45 +0800

    Add content and links in Game1.js (frogger)

--- src/projects/Game1.js
+++ src/projects/Game1.js
@@ -8,7 +8,6 @@ import {
 
 
 class Game1 extends Component {
-    // contextRef = createRef()
 
     render(){
         return(
@@ -16,7 +15,6 @@ class Game1 extends Component {
                 <Segment.Group className="projectNotes">
                     <PortfolioNotes/>
 
-                    {/* sticky navigation on the left */}
                     <Rail position="left" className="navRail"> 
                         <LeftNavigation/>
                     </Rail>
@@ -30,46 +28,36 @@ class Game1 extends Component {
 const PortfolioNotes = () => (
     <div class="lightPadding">
         <Header
-            content = "Would You Rather Apps"
+            content = "Frogger Arcade Game"
             textAlign = "center"
             as = "h1"
         />
+        <p className="subHeader"><span><a href="https://revin-frogger-game.netlify.app">Live Demo</a></span>  ||  <span><a href="https://github.com/rtanri/Frogger-Game" target="_blank">Github Repo</a></span></p>
 
         <Segment>
             <Header as="h3">1. Project Requirement</Header>
             <Header as="h4">1.1. Simple Overview</Header>
             <Overview />
             <Header as="h4">1.2. App Functionality</Header>
-            <LorumIpsum />
-            <Screenshot />
+            <AppF12y />
+            <Screenshot source="/images/dashboard/frogger.jpg" />
             <Header as="h4">1.3. Workflow Process</Header>
-            <LorumIpsum />
+            <Workflow />
         </Segment>
 
         <Segment>
-            <Header as="h3">2. App Design</Header>
-            <Header as="h4">2.1. View</Header>
+            <Header as="h3">2. Coding Phase</Header>
+            <Header as="h4">2.1. API Function</Header>
             <LorumIpsum />
-            <Header as="h4">2.2. Component Hierarcy</Header>
+            <Header as="h4">2.2. Action & Reducers</Header>
             <LorumIpsum />
             <Screenshot />
-            <Header as="h4">2.3. Store Data</Header>
+            <Header as="h4">2.3. Middleware</Header>
             <LorumIpsum />
         </Segment>
 
         <Segment>
-            <Header as="h3">3. Coding Phase</Header>
-            <Header as="h4">3.1. API Function</Header>
-            <LorumIpsum />
-            <Header as="h4">3.2. Action & Reducers</Header>
-            <LorumIpsum />
-            <Screenshot />
-            <Header as="h4">3.3. Middleware</Header>
-            <LorumIpsum />
-        </Segment>
-
-        <Segment>
-            <Header as="h3">4. Live Demo (Click here)</Header>
+        <Header as="a" href="https://revin-frogger-game.netlify.app" target='_blank' className="childHeader">3. Live Demo <span>✨</span></Header>
         </Segment>
 
     </div>
@@ -84,19 +72,14 @@ const LeftNavigation = () => (
             <Header.Subheader>1.2. App Functionality </Header.Subheader>
             <Header.Subheader>1.3. Workflow process </Header.Subheader>
 
-            <Header as="h4">2. App Design</Header>
-            <Header.Subheader>2.1. View</Header.Subheader>
-            <Header.Subheader>2.2. Component Hierarcy</Header.Subheader>
-            <Header.Subheader>2.2. Store Data</Header.Subheader>
+            <Header as="h4">2. Coding Phase</Header>
+            <Header.Subheader>2.1. API Functions</Header.Subheader>
+            <Header.Subheader>2.2. Actions & Reducers</Header.Subheader>
+            <Header.Subheader>2.3. Middleware</Header.Subheader>
+            <Header.Subheader>2.4. Initialize App Data</Header.Subheader>
 
-            <Header as="h4">3. Coding Phase</Header>
-            <Header.Subheader>3.1. API Functions</Header.Subheader>
-            <Header.Subheader>3.2. Actions & Reducers</Header.Subheader>
-            <Header.Subheader>3.3. Middleware</Header.Subheader>
-            <Header.Subheader>3.4. Initialize App Data</Header.Subheader>
-            <Header.Subheader>3.4. Login Page</Header.Subheader>
+            <Header as="h4"></Header>
 
-            <Header as="h4">4. Finished: Live Demo</Header>
         </Segment>
     </div>
 )
@@ -105,10 +88,7 @@ const LorumIpsum = () => (
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel nisl at leo 
     tempor maximus eget quis velit. Vivamus tincidunt imperdiet suscipit. Donec dapibus 
     quis magna ac porta. Etiam id tempus tellus, ornare consectetur tortor. Praesent porta 
-    urna interdum arcu gravida, et luctus nulla maximus. Praesent ut viverra sem, eget ultricies nisi. 
-    Fusce a sagittis tortor, ac maximus nibh. Phasellus id justo eu nulla volutpat tempus. 
-    Nullam nunc ante, iaculis a cursus eu, placerat id risus. Donec tincidunt scelerisque augue at iaculis. 
-    Sed vel leo eu tellus iaculis vehicula.
+    urna interdum arcu gravida, et luctus nulla maximus. 
     </p>
 )
 
@@ -116,27 +96,51 @@ const LorumIpsum = () => (
 const Overview =() => (
     <div>
     <p>
-         The game board consists of sixteen "cards" arranged in a grid. The deck is made up of 
-         eight different pairs of cards, each with different symbols on one side. The cards are 
-         arranged randomly on the grid with the symbol face down. The gameplay rules are 
-         very simple: flip over two hidden cards at a time to locate the ones that match!
-         Each turn:
+    You will be provided visual assets and a game loop engine; 
+    using these tools you must add a number of entities to the 
+    game including the player characters and enemies to recreate the 
+    classic arcade game Frogger. This game is a good practice for Object Oriented Programming.
          </p>
- 
-         <ul> 
-             <li>The player flips one card over to reveal its underlying symbol.</li>
-             <li>The player then turns over a second card, trying to find the corresponding card with the same symbol.</li>
-             <li>If the cards match, both cards stay flipped over. </li>
-             <li>If the cards do not match, both cards are flipped face down. </li>
-             <li>The game ends once all cards have been correctly matched. </li>
-         </ul>
      </div>
  )
 
-
-const Screenshot = () => (
-    <Image size="large" src="/images/wireframe/image.png" />
+const AppF12y = () => (
+    <div>
+        <p>
+        In this game you have a Player and Enemies (bugs). 
+        The goal of the player is to reach the water, without 
+        colliding into any one of the enemies.
+        </p>
+        <ul>
+            <li>The player can move left, right, up and down; press 'space' to change character</li>
+            <li>The enemies move at varying speeds on the paved block portion of the game board</li>
+            <li>Once a the player collides with an enemy, the game is reset and the player moves back to the starting square</li>
+            <li>Once the player reaches the water-on-top-list, score will be added +20pts.</li>
+            <li>Game finishes when player got hit by the bugs 3 times.</li>
+        </ul>
+    </div>
 )
+
+const Workflow = () => (
+    <div>
+    <p>
+        Create 'Player' and 'Enemies' classes using Object Oriented Programming.
+    </p>
+        <ol>
+            <li>Set the 'Enemies' props (image, speed, initial locations, movement direction),</li>
+            <li>Set the logic on handling Collision with 'Player',</li>
+            <li>Set the 'Player' props (initial location, image, handleInput from key pressed),</li>
+            <li>Add the logic of 'Adding Score', 'Decreasing Life', and 'End-Game'</li>
+        </ol>
+    </div>
+
+)
+
+
+const Screenshot = ({source}) => (
+    <Image size="large" src={source}/>
+)
+
 
 
 export default Game1
